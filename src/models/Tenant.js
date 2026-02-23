dfe573126250bb53bfcf1406b0ff429c7b2422c4
abdulkareemmain <abdulkareemmain@gmail.com>
2026-02-23 18:33:42 +0400

    fix: update Tenant model to match migration

--- src/models/Tenant.js
+++ src/models/Tenant.js
@@ -12,8 +12,9 @@ module.exports = (sequelize, DataTypes) => {
         primaryKey: true,
         autoIncrement: true,
       },
-      name: {
-        type: DataTypes.STRING(200),
+      tenant_code: {
+        type: DataTypes.STRING(50),
+        unique: true,
         allowNull: false,
       },
       company_name: {
